6cb46c6e5900d936d895a67f0f94ccad19c0d9fa
Christian <christian_nogueras94@hotmail.com>
2017-08-19 13:23:22 -0700

    second commit

--- public/vendor.bundle.js
+++ public/vendor.bundle.js
@@ -33656,6 +33656,7 @@ exports.root = _root;
 /* 26 */
 /***/ (function(module, exports) {
 
+<<<<<<< HEAD
 var g;
 
 // This works in non-strict mode
@@ -33677,6 +33678,29 @@ try {
 // easier to handle this case. if(!global) { ...}
 
 module.exports = g;
+=======
+var g;
+
+// This works in non-strict mode
+g = (function() {
+	return this;
+})();
+
+try {
+	// This works if eval is allowed (see CSP)
+	g = g || Function("return this")() || (1,eval)("this");
+} catch(e) {
+	// This works if the window reference is available
+	if(typeof window === "object")
+		g = window;
+}
+
+// g can still be undefined, but nothing to do about it...
+// We return undefined, instead of nothing here, so it's
+// easier to handle this case. if(!global) { ...}
+
+module.exports = g;
+>>>>>>> testing commit from window
 
 
 /***/ }),
