875afeb212c6e548b233f3e6275779232c636b2d
Christian Nogueras <Christiannogueras@MBPdeChristian.lan>
2017-12-08 13:29:01 -0400

    appdynamic

--- config/appDynamic.js
+++ config/appDynamic.js
@@ -1,6 +1,7 @@
 /*What is appdynamic?.Apdynamic es un software que me ayuda a verifcar el tiempo que se tarda un request en llegar al cliente. Entre mucha otras cosas mas pero precisamente lo uso para validar el tiempo de espera del cliente en la pagina web
  *
 */
+console.log("AppDynamic")
 // special configuration for AppDynamic
 require("appdynamics").profile({
     controllerHostName: 'hopper2017120806294145.saas.appdynamics.com',
@@ -11,6 +12,6 @@ require("appdynamics").profile({
     accountName: 'hopper2017120806294145',
     accountAccessKey: '01mcepvf95e0',
     applicationName: 'Opas Production',
-    tierName: 'Opas',
+    tierName: 'Opas Production',
     nodeName: 'process' // The controller will automatically append the node name with a unique number
    });
