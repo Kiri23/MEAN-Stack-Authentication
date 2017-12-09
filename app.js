ca3241dfcc99ed91692d06bb1ac41279d10bc962
Christian Nogueras <Christiannogueras@MBPdeChristian.lan>
2017-12-08 21:54:55 -0400

    cambios

--- app.js
+++ app.js
@@ -215,47 +215,40 @@ function sendMessageToSlack(message){
 
 variables.app.post('/slashComand',(req,res)=>{
   console.log("form localhos")
-  console.log(req.body)
-  var reqBody = req.body
-  var responseURL = reqBody.response_url
-  if (reqBody.token != process.env.slack_verification_token){
-      res.status(403).end("Access forbidden")
-  }else{
-      var message = {
-          "text": "This is your first interactive message",
-          "attachments": [
-              {
-                  "text": "Building buttons is easy right?",
-                  "fallback": "Shame... buttons aren't supported in this land",
-                  "callback_id": "button_tutorial",
-                  "color": "#3AA3E3",
-                  "attachment_type": "default",
-                  "actions": [
-                      {
-                          "name": "yes",
-                          "text": "yes",
-                          "type": "button",
-                          "value": "yes"
-                      },
-                      {
-                          "name": "no",
-                          "text": "no",
-                          "type": "button",
-                          "value": "no"
-                      },
-                      {
-                          "name": "maybe",
-                          "text": "maybe",
-                          "type": "button",
-                          "value": "maybe",
-                          "style": "danger"
-                      }
-                  ]
-              }
-          ]
-      }
-      sendMessageToSlackResponseURL(responseURL, message)
+  var message = {
+      "text": "This is your first interactive message",
+      "attachments": [
+          {
+              "text": "Building buttons is easy right?",
+              "fallback": "Shame... buttons aren't supported in this land",
+              "callback_id": "button_tutorial",
+              "color": "#3AA3E3",
+              "attachment_type": "default",
+              "actions": [
+                  {
+                      "name": "yes",
+                      "text": "yes",
+                      "type": "button",
+                      "value": "yes"
+                  },
+                  {
+                      "name": "no",
+                      "text": "no",
+                      "type": "button",
+                      "value": "no"
+                  },
+                  {
+                      "name": "maybe",
+                      "text": "maybe",
+                      "type": "button",
+                      "value": "maybe",
+                      "style": "danger"
+                  }
+              ]
+          }
+      ]
   }
+    sendMessageToSlack(message)
 
 })
 
