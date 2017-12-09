d0344d7689912fa0f66ebf22cc8315ec212ee56c
Christian Nogueras <Christiannogueras@MBPdeChristian.lan>
2017-12-08 23:10:31 -0400

    cambios

--- app.js
+++ app.js
@@ -256,10 +256,13 @@ variables.app.post('/slashComand',(req,res)=>{
 
 })
 
-variables.app.post("/incomingSlackMessageAction",(req,res)=>{
+var urlencodedParser = modules.bodyParser.urlencoded({ extended: false })
+
+variables.app.post("/incomingSlackMessageAction",urlencodedParser,(req,res)=>{
+  res.status(200).end()
   console.log("Hello baby")
   console.log(req.body)
-  res.send(req.body)
+  // res.send(req.body)
 });
 
 function sendMessageToSlackResponseURL(responseURL, JSONmessage){
