263980f11c619654968c5826b066c5572bfd3f75
Christian Nogueras <Christiannogueras@MBPdeChristian.lan>
2017-12-09 09:51:44 -0400

    cambios

--- app.js
+++ app.js
@@ -208,40 +208,40 @@ variables.app.post('/slashComand',urlencodedParser,(req,res)=>{
   if (reqBody.token != process.env.slack_verifcation_token){
       res.status(403).end("Access forbidden")
   }else{
-      var message = {
-          "text": "This is your first interactive message",
-          "attachments": [
-              {
-                  "text": "Building buttons is easy right?",
-                  "fallback": "Shame... buttons aren't supported in this land",
-                  "callback_id": "button_tutorial",
-                  "color": "#3AA3E3",
-                  "attachment_type": "default",
-                  "actions": [
-                      {
-                          "name": "yes",
-                          "text": "yes",
-                          "type": "button",
-                          "value": "yes"
-                      },
-                      {
-                          "name": "no",
-                          "text": "no",
-                          "type": "button",
-                          "value": "no"
-                      },
-                      {
-                          "name": "maybe",
-                          "text": "maybe",
-                          "type": "button",
-                          "value": "maybe",
-                          "style": "danger"
-                      }
-                  ]
-              }
-          ]
-      }
-    }
+    var message = {
+      "text": "This is your first interactive message",
+      "attachments": [
+          {
+              "text": "Building buttons is easy right?",
+              "fallback": "Shame... buttons aren't supported in this land",
+              "callback_id": "button_tutorial",
+              "color": "#3AA3E3",
+              "attachment_type": "default",
+              "actions": [
+                  {
+                      "name": "yes",
+                      "text": "yes",
+                      "type": "button",
+                      "value": "yes"
+                  },
+                  {
+                      "name": "no",
+                      "text": "no",
+                      "type": "button",
+                      "value": "no"
+                  },
+                  {
+                      "name": "maybe",
+                      "text": "maybe",
+                      "type": "button",
+                      "value": "maybe",
+                      "style": "danger"
+                  }
+              ]
+          }
+      ]
+  }
+}
 
 
     console.log("llego aca")
