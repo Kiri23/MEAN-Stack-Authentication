1e3c0e893110a50809c1dec5476e5772cab4beb2
Christian Nogueras <Christiannogueras@MBPdeChristian.lan>
2017-12-29 01:21:54 -0400

    lo arregle ahora usuario y administrador pueden ver su informacion en el perfil

--- config/passport.js
+++ config/passport.js
@@ -3,6 +3,7 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 const User = require('../models/user');
+const Administrator = require('../models/administrator');
 const config = require('./database');
 
 module.exports = function(passport){
@@ -14,13 +15,18 @@ module.exports = function(passport){
   passport.use(new JwtStrategy(opts,(jwt_payload,done) => {
     console.log("---------- \n payload down")
     console.log(jwt_payload);
+    // por el momento solo funciona con usuarioa regulares. Tengo que hacer que tambien busque por administradores
     User.getUserById(jwt_payload._doc._id, (err,user) => {
       if (err){
         return done(err,false);
       }
       if(user){
         return done(null,user);
-      }else {
+      }if (!user){
+        console.log("Buscando autenticidad con administrador. Passport.js config File node Backend")
+        searchAdministrator(jwt_payload,done)
+      }
+      else {
         return done(null,false);
       }
 
@@ -28,3 +34,18 @@ module.exports = function(passport){
   }));
 
 };
+
+// Tengo que buscar en la base de administradores y ver si existen para poderlo autenticar. tengo que hacer el mismo procceso de arriba ver si hay errores y devolver falsos, ver si encontre un administradore y resolver o cualquier otra cosa enviar acceso unathorized(false)
+function searchAdministrator(jwt_payload,done){
+    Administrator.getAdministratorById(jwt_payload._doc._id,(err,administrator)=>{
+      if (err){
+        console.log("Error en passport.js config file funcion del administrador. Hubo un error autenticado el administrador me devolvio un error la llamada a la base de datos. error - " + err )
+        return done(err,false)
+      }if (administrator){
+        return done(null,administrator)
+      }else{
+        return done(null,false)
+      }
+    })
+  
+}
